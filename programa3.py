4a698b62801f9cb6a1de6d6e6db107ab60b34ddc
leyliperea <160614703+leyliperea@users.noreply.github.com>
2024-02-22 08:06:03 -0600

    Create programa3.py

---
+++ programa3.py
@@ -0,0 +1,19 @@
+    Nombre:Leylani pg
+    matricula:1723110391
+    grupo:TI21
+    Fecha:18/01/2024
+    Descripcion:programa que crrea un objeto
+
+"""
+
+class A:#se crea la clase
+    matricula:None#atributo
+    nombre:None#atributo
+objetoA = A () #se crea el objeto
+print(objetoA.matricula)#se imprime el atributo
+def __init__(self): #se crea el metodo constructor
+    self.nombre = "None"#atributo
+    print("contructo de la clase A")#se imprime atributo
+
+objetoA= A()#se crea objeto
+  print(objetoA.nombre)#se imprime el atributo
