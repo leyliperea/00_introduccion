401d09abde7bede62fe8b7f8be72c062184990dc
leyliperea <160614703+leyliperea@users.noreply.github.com>
2024-02-22 08:07:07 -0600

    Create programa04.py

---
+++ programa04.py
@@ -0,0 +1,15 @@
+    Nombre:Leylani pg
+    matricula:1723110391
+    grupo:TI21
+    Fecha:18/01/2024
+    Descripcion:programa que crrea un objeto
+
+"""
+
+class Alumnos:#crea la clase
+    matricula:None#atributos
+    nombre:None#atributos
+    def __init__(self,matricula,nombre):#constructor
+        self.matricula=None#atributos
+        self.nombre =None#atributos
+        print("objeto alumnos")#imprime al objeto
